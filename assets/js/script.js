41a8605b8513056751aaeacf1054d11ee51c2f53
Johnnyfromtheblock4 <simo.visco4@gmail.com>
2025-07-21 19:45:48 +0200

    [UPD] fix-html

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,8 +10,9 @@ axios.get("https://lanciweb.github.io/demo/api/pictures/").then((resp) => {
   //ciclo per i 6 elementi
   for (let i = 0; i < 6; i++) {
     cardBox.innerHTML += `<div class="carta col-12 col-md-6 col-lg-4 mt-5 p-4">
-        <img src="assets/img/pin.svg" alt="" class="pin img-fluid" />
+        <img src="assets/img/pin.svg" alt="" class="pin" />
         <img src="${card[i].url}" alt="" class="img-fluid" />
+        <h3 class="mt-3 mb-3">${card[i].title}</h3>
         <p class="mt-3 mb-3">
           ${card[i].date}
         </p>
